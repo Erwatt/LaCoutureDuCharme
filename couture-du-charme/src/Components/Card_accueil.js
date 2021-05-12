45e830e543a9f490fd287663486db3ae994203de
Nicolas <nicolas.defoort@isen.yncrea.fr>
2021-05-12 09:45:49 +0200

    update HomePage

--- couture-du-charme/src/Components/Card_accueil.js
+++ couture-du-charme/src/Components/Card_accueil.js
@@ -9,11 +9,26 @@ export default function CardAccueil(props){
 
     return(
         
-        <div class="accueilcard" >
-            {console.log(isClicked)}
-            <div class="titleAccueilCard">{props.Titre}</div>
-            {isClicked?<div class="insidecard"><img class="imgCard" src={props.img}></img><button onClick={() => setClick(false)}>En savoir plus</button></div>:<div class="insidecard"><div class="descriptionAccueilCard">{props.description}</div><button type="button" onClick={() => setClick(true)}>Retour</button></div>}
+        
+           
+        <>
+            {isClicked?<div class="accueilcardBg" >
+           
             
-        </div>
+                <div class="titleAccueilCard">{props.Titre}</div>
+                <button class="buttonCard" onClick={() => setClick(false)}>En savoir plus</button>
+            
+            </div>:
+            <div class="accueilcard" >
+            
+                <div class="titleAccueilCard">{props.Titre}</div>
+                <div class="descriptionAccueilCard">{props.description}</div>
+                <button class="buttonCard" type="button" onClick={() => setClick(true)}>Retour</button>
+            </div>
+            
+            }
+            
+        </>
     )
 }
+
